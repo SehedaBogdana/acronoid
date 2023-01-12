fbd106bae1dea4a7559f7561d6530febd7e8b0a9
Anastasiiasyvak <asyvak@kse.org.ua>
2023-01-12 17:59:56 +0200

    new_commit

--- main.py
+++ main.py
@@ -1,34 +1,71 @@
 import pgzrun
 from new_vector import Vector
-from actors import Ball, Life, Obstacle, Paddle # импортируем классы игровых актеров
+from pgzero.actor import Actor
 
 WIDTH = 800
 HEIGHT = 800
-#
-# #----------------------------------------------------------------------------------------------------------
-# # Создаем объекты игровых актеров
-# paddle = Paddle(300, 785)  # ракетка с начальными координатами
-# ball = Ball(100, 300)  # мяч с начальными координатами
-#
-#
-# #----------------------------------------------------------------------------------------------------------
-# # Функция, отвечающая за отрисовку игрового поля
-# def draw():
-#     pass
-#
-# #----------------------------------------------------------------------------------------------------------
-# # Функция, отвечающая за логику игры
-# def update():
-#     pass
-# #----------------------------------------------------------------------------------------------------------
-# # Функция, отвечающая за передвижение ракетки при движении мыши
-# def on_mouse_move(pos):
-#     # paddle.x = pos[0]  # изменяем координату ракетки по оси X
-#     pass
-# #----------------------------------------------------------------------------------------------------------
-# # Функция, отвечающая за потерю жизни при вылете мяча
-# def lose_life():
-#     pass
-#
+b = 8
+a = 5
+hearts = [Actor("heart", center=(30, 50)), Actor("heart", center=(80, 50)), Actor("heart", center=(130, 50))]
+size_heart = len(hearts)
+
+
+class Ball:
+    def __init__(self, vector: Vector):
+        self.position = vector
+
+
+class Puddle:
+    def __init__(self, vector: Vector, size: Vector):
+        self.position = vector
+        self.size = size
+
+
+puddle1 = Puddle(Vector(100, 770), Vector(200, 30))
+
+circle = Ball(Vector(100, 25))
+
+
+def draw():
+    global size_heart
+    screen.clear()
+    screen.fill("peru")
+    screen.draw.filled_circle((circle.position.x, circle.position.y), 25, "darkred")
+    screen.draw.filled_rect((Rect((puddle1.position.x, puddle1.position.y), (puddle1.size.x, puddle1.size.y))), "black")
+    for heart in hearts:
+        heart.draw()
+    if circle.position.y > 800:
+        size_heart -= 1
+
+
+def update(dt):
+
+    # # b рух кульки по вертикалі
+    global size_heart
+    global a
+    circle.position.x = circle.position.x + a
+    if circle.position.x > (WIDTH - 25) or circle.position.x < 25:
+        a = - a
+    global b
+    circle.position.y = circle.position.y + b
+    if circle.position.y <= 770 <= (circle.position.y + 25) and puddle1.position.x <= circle.position.x <= (puddle1.position.x + 200) or circle.position.y < 25:
+        b = -b
+    if circle.position.y > 800:
+        hearts.pop()
+        circle.position = Vector(100, 100)
+
+    #     size_heart -= 1
+
+# по горизонталі x pos[0]
+# по вертикалі y pos[1]
+
+
+def on_mouse_move(pos):
+    new_x = pos[0]
+    if new_x > WIDTH - puddle1.size.x:
+        new_x = WIDTH - puddle1.size.x
+    puddle1.position.x = new_x
+    print(puddle1.position)
+
 
 pgzrun.go()
